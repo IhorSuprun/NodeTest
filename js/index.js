5c99144c2d879d1e95e7a8f03560070794369d73
ihor <sup.on.php@gmail.com>
2018-12-16 21:13:37 +0200

    cron

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
-console.log('Running');
+//const cron = require('node-cron');
+
+//cron.schedule('*/2 * * * * *', () => {
+//    console.log(Math.floor(Date.now() / 1000));
+//});
--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const http = require('http');
 
 const server = http.createServer();
 
+const cron = require('node-cron');
+
+const result= cron.schedule('*/2 * * * * *', () => {
+    console.log(Math.floor(Date.now() / 1000));
+});
+
 server.on('request', (req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('All OK');
 });
 
-server.listen(8000, () => console.log('Running'));
+server.listen(8000, () => console.log('Running' + result));
