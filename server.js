6da4cd02a82bd7415c4944cffab0ca0c8b4d19f1
ihor <sup.on.php@gmail.com>
2018-12-24 19:09:56 +0200

    weather API in cron

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ const http = require('http');
 
 const server = http.createServer();
 
+const axios = require('axios');
+
 const cron = require('node-cron');
 
-const result= cron.schedule('*/2 * * * * *', () => {
-    console.log(Math.floor(Date.now() / 1000));
+const result= cron.schedule('*/10 * * * * *', () => {
+    return(
+        axios.get('https://api.openweathermap.org/data/2.5/weather?q=Dnipro,ua&units=metric&APPID=38fc1678aa4a045c04c1393432877266')
+            .then(function (response) {
+                console.log(response.data.main);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    );
 });
 
 server.on('request', (req, res) => {
